bb1e6efafef297b7b6cadf10800551d68f9985e2
tbw <zhaoyuansong563@gmail.com>
2024-02-27 23:55:16 +0800

    lab1-3 hw3

---
+++ fa21-main/hw/hw3/tests/q3bii.py
@@ -0,0 +1,2 @@
+OK_FORMAT = True
+test = {'name': 'q3bii', 'points': 2, 'suites': [{'cases': [{'code': '>>> invalid_bid_count >= 0 \nTrue', 'hidden': False, 'locked': False}], 'scored': True, 'setup': '', 'teardown': '', 'type': 'doctest'}]}
---
+++ fa21-main/lab/lab03/tests/q4.py
@@ -0,0 +1,8 @@
+OK_FORMAT = True
+test = {   'name': 'q4',
+    'points': None,
+    'suites': [   {   'cases': [{'code': '>>> answer4 == "SEX CRIME"\nTrue', 'hidden': False, 'locked': False}, {'code': ">>> plt.close('all')\n", 'hidden': False, 'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
